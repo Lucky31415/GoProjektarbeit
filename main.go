58cfafb84e3431bf18895e712cdd551ec32180cb
Lukas von Essen <lukas.von.essen@googlemail.com>
2022-03-25 09:33:53 +0100

    Cleanup

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	//"fmt"
-
-	//. "github.com/Lucky31415/GoProjektarbeit/Expressions"
 	. "github.com/Lucky31415/GoProjektarbeit/Tests"
 )
 
 func main() {
 	TestParserGood()
 	TestVM()
-	//me := Expressions.NewMultExp(Expressions.NewIntExp(1), Expressions.NewPlusExp(Expressions.NewIntExp(3), Expressions.NewIntExp(5)))
-	//fmt.Printf("%s = %d", me.Pretty(), me.Eval())
-
-	//unitTest1()
-	//test1()
-	//rz1 := ScanRZ()
-	//rz2 := ScanRZ()
-	//
-	//rzSum := AddRz(rz1, rz2)
-	//PrintRZ(rzSum)
-
 }
